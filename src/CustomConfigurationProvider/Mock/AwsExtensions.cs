e901327d57be66053d493c05a6c98c865218c93e
Pieter Samyn <26283753+PhbsSmn@users.noreply.github.com>
2024-11-18 23:30:47 +0100

    make sure the aws call is intercepted to have no unpleasant surprises

---
+++ src/CustomConfigurationProvider/Mock/AwsExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    file sealed class InterceptsLocationAttribute : Attribute
+    {
+        public InterceptsLocationAttribute(string filePath, int line, int column)
+        {
+        }
+    }
+}
+
+namespace ConfigurationProvider.Mock
+{
+    public static class AwsExtensions
+    {
+        [System.Runtime.CompilerServices.InterceptsLocation(@"C:\Presentations\configuration-and-options\src\CustomConfigurationProvider\Configuration\Aws\AwsSecretsManagerConfigurationProvider.cs", 48, 39)]
+        internal static Task<GetSecretValueResponse> GetSecretValueAsync(this AmazonSecretsManagerClient client,
+            GetSecretValueRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(new GetSecretValueResponse()
+            {
+                SecretString = """
+                {
+                    "AppSettings": {
+                        "Setting": "SecretsManagerValue"
+                    }
+                }
+                """
+            });
+        }
+    }
+}
--- src/CustomConfigurationProvider/Program.cs
+++ src/CustomConfigurationProvider/Program.cs
@@ -1,11 +1,14 @@
 using CustomConfigurationProvider.Configuration.Aws;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddAwsSecretsManager("Test");
+//builder.Configuration.AddAwsSecretsManager("Test");
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 
-// TODO map get for the configuration
+app.MapGet("/appsettings", (IConfiguration configuration) =>
+{
+    return configuration.GetSection("appsettings").GetChildren().Select(x => $"{x.Key}={x.Value}").ToArray();
+});
 
 app.Run();
